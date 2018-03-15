2a8357f7f240a3a24b6e32212caa4ebea4512531
coldblood101 <35728192+coldblood101@users.noreply.github.com>
2018-03-15 18:45:35 +0700

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -3,13 +3,14 @@ using namespace std;
 //declear the static member 
 SDL_Renderer* Game::renderer = nullptr;
 SDL_Event Game::e;
-
+int Game::Xbullet[20];
+int Game::Ybullet[20];
 
 
 //declear the map pointer 
 Map* map = nullptr;
 Player* man = nullptr;
-enemy* monster = nullptr;
+enemy* monster[10];
 
 Game::Game()
 {
@@ -42,10 +43,17 @@ void Game::Initialize(const char* title, int x, int y, int width, int height, in
 		gameEnd = true;
 		return;
 	}
+	for (int i = 0; i < 20; i++)
+	{
+		Xbullet[i] = 0;
+		Ybullet[i] = 0;
+	}
 	//declear the pointer value 
 	map = new Map(0);
 	man = new Player(7);
-	monster = new enemy(500, 500);
+	for (int i = 0; i < 10; i++) {
+		monster[i] = new enemy(50+200*i,50+200*i);
+	}
 
 
 	return;
@@ -57,9 +65,9 @@ void Game::Draw()
 	//Draw the game 
 	map->Draw();
 	man->Draw();
-	monster->Draw();
-
-
+	for (int i = 0; i < 10; i++)
+		if (monster[i] != nullptr)
+			monster[i]->Draw();
 
 	///////////////////
 	SDL_RenderPresent(renderer);
@@ -70,7 +78,20 @@ bool Game::isRunning() {
 void Game::Update()
 {
 	man->Update();
-	monster->Update();
+	for (int i = 0; i < 10; i++){
+		if (monster[i] != nullptr) {
+			monster[i]->Update();
+			for (int j = 0; j < 20; j++)
+			{
+				if (monster[i]->bulletHit(Xbullet[j], Ybullet[j]))
+					monster[i]->heart -= 1;
+			}
+			if (monster[i]->heart <= 0)
+				monster[i] = nullptr;
+			
+			
+			}
+		}
 }
 void Game::Close()
 {
